f052668fd511293f6c22229948781b10571570a8
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-07 18:39:23 +0530

    Update CardList.jsx

--- src/app/components/cardList/CardList.jsx
+++ src/app/components/cardList/CardList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from './cardList.module.css';
+import Card from "../card/Card";
 
 export default function CardList() {
     return (
         <div className={styles.container}>
-            CardList
+            <h1 className={styles.title}>Recent Posts</h1>
+            <div style={styles.posts}>
+                <Card/>
+                <Card/>
+            </div>
         </div>
     )
 }
