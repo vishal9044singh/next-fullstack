ce3dd32811cf495cf69ce21ab8b61528517e32b3
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-08 19:12:53 +0530

    Create blogPage.jsx

---
+++ src/app/components/blogPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CardList from "../components/cardList/CardList";
+import Menu from "../components/menu/Menu";
+import styles from './blogPage.module.css';
+
+export default function BlogPage() {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>Style Blog</div>
+            <div className={styles.content}>
+             <CardList/>
+             <Menu/>
+            </div>
+        </div>
+    )
+}
